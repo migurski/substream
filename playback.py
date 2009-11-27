6d7477fb806ea35c8ecbc2e6e06152a2f63d5b20
Michal Migurski <migurski@Philippe.local>
2009-11-26 22:54:40 -0800

    Pulling everything out of redis at the other end

---
+++ playback.py
@@ -0,0 +1,12 @@
+import json
+import redis
+
+if __name__ == '__main__':
+    store = redis.Redis()
+    
+    while store.llen('stream'):
+        key = store.pop('stream')
+        tweet = json.loads(store.get(key))
+        store.delete(key)
+    
+        print '%(screen_name)20s - %(text)s' % tweet
